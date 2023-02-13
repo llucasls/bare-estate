01e32260174278c61a7f121f290452b4f27c75a2
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-02-13 02:02:50 -0300

    revert: erase module description

--- bare.py
+++ bare.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python
-"""
-function bare --wraps='git --git-dir=/home/lucas/.dotfiles/ --work-tree=/home/lucas' --description 'manage dotfiles using a bare repository'
-  git --git-dir=/home/lucas/.dotfiles/ --work-tree=/home/lucas $argv; 
-end
-"""
 import sys
 import subprocess as sp
 
