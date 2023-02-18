c932f7c26c016a01eca7f7474960b7053826da92
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-02-18 04:08:06 -0300

    feat: clone dotfiles directory

--- new_dir.py
+++ new_dir.py
@@ -2,15 +2,30 @@ import os
 import tempfile
 import subprocess as sp
 
+from config import configs, HOME
+from commands import bare_cmd
 
-with tempfile.TemporaryDirectory() as tmp_dir1:
-    print("created directory", tmp_dir1)
 
-    command = ["git",
-               "clone",
-               "https://github.com/llucasls/netbsd-home.git",
-               f"{tmp_dir1}/dotfiles"]
+with tempfile.TemporaryDirectory() as tmp_dir:
+    print("created directory", tmp_dir)
 
-    sp.run(command, stdout=sp.DEVNULL)
+    clone_command = ["git",
+                     "clone",
+                     "--quiet",
+                     f"--separate-git-dir={configs['history_location']}",
+                     "https://github.com/llucasls/netbsd-home.git",
+                     f"{tmp_dir}/dotfiles"]
 
-    print(os.listdir(f"{tmp_dir1}/dotfiles"))
+    rsync_command = ["rsync",
+                     "--recursive",
+                     "--verbose",
+                     "--exclude",
+                     ".git",
+                     f"{tmp_dir}/dotfiles/",
+                     f"{HOME}/"]
+
+    config_command = [*bare_cmd, "config", "status.showUntrackedFiles", "no"]
+
+    sp.run(clone_command)
+    sp.run(rsync_command)
+    sp.run(config_command)
