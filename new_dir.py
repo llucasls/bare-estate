39e2475740e218c12704f04ee6348895a531d0e4
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-02-17 23:55:20 -0300

    chore: clone repo into temporary directory

--- new_dir.py
+++ new_dir.py
@@ -1,9 +1,16 @@
+import os
 import tempfile
+import subprocess as sp
 
 
 with tempfile.TemporaryDirectory() as tmp_dir1:
     print("created directory", tmp_dir1)
 
+    command = ["git",
+               "clone",
+               "https://github.com/llucasls/netbsd-home.git",
+               f"{tmp_dir1}/dotfiles"]
 
-tmp_dir2 = tempfile.mkdtemp()
-print("created directory", tmp_dir2)
+    sp.run(command, stdout=sp.DEVNULL)
+
+    print(os.listdir(f"{tmp_dir1}/dotfiles"))
