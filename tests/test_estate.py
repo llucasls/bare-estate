1fe9c377cd815814d575955ee94f81e3e334a0dc
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-03-15 16:10:58 -0300

    test: create test file for estate command

---
+++ tests/test_estate.py
@@ -0,0 +1,11 @@
+import subprocess as sp
+
+
+ESTATE = "bare_estate/cli.py"
+
+
+def test_cli_status_when_repo_doesnt_exist():
+    stderr = b"Error: the repository has not been initialized yet.\nYou can create a new repository using the command:\n\nbare init\n"
+    result = sp.run([ESTATE, "status"], stdout=sp.PIPE, stderr=sp.PIPE)
+    assert result.returncode == 2
+    assert result.stderr == stderr
