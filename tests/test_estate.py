534bc7827dd517d68d96bc0588ee68c0e982e27a
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-04-02 19:53:15 -0300

    test: test with new script name

--- tests/test_estate.py
+++ tests/test_estate.py
@@ -7,7 +7,7 @@ ESTATE = "bare_estate/cli.py"
 def test_cli_status_when_repo_doesnt_exist():
     "Test CLI Application :: should error when it doesn’t receive an argument"
 
-    stderr = b"Error: the repository has not been initialized yet.\nYou can create a new repository using the command:\n\nbare init\n"
+    stderr = b"Error: the repository has not been initialized yet.\nYou can create a new repository using the command:\n\nestate init\n"
     result = sp.run([ESTATE, "status"], stdout=sp.PIPE, stderr=sp.PIPE)
     assert result.returncode == 2
     assert result.stderr == stderr
