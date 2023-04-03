4f10a8d9a1052c3f1f6f015f459001fd2f178feb
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-04-03 14:30:30 -0300

    test: run application without arguments
    Also correct the docstring of the previously written test.

--- tests/test_estate.py
+++ tests/test_estate.py
@@ -4,8 +4,17 @@ import subprocess as sp
 ESTATE = "bare_estate/cli.py"
 
 
-def test_cli_status_when_repo_doesnt_exist():
+def test_run_without_arguments():
     "Test CLI Application :: should error when it doesn’t receive an argument"
+
+    stderr = b"Error: no command was provided to git\n"
+    result = sp.run([ESTATE], stdout=sp.PIPE, stderr=sp.PIPE)
+    assert result.returncode == 4
+    assert result.stderr == stderr
+
+def test_cli_status_when_repo_doesnt_exist():
+    """Test CLI Application ::
+    should error when repository hasn’t been initialized yet"""
 
     stderr = b"Error: the repository has not been initialized yet.\nYou can create a new repository using the command:\n\nestate init\n"
     result = sp.run([ESTATE, "status"], stdout=sp.PIPE, stderr=sp.PIPE)
