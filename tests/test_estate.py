29ec7d9dd4d699a96ad23c01e6f65d8ab694a70e
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-04-11 17:53:59 -0300

    test: reorder assertions

--- tests/test_estate.py
+++ tests/test_estate.py
@@ -9,8 +9,8 @@ def test_run_without_arguments():
 
     stderr = b"Error: no command was provided to git\n"
     result = sp.run([ESTATE], stdout=sp.PIPE, stderr=sp.PIPE)
-    assert result.returncode == 4
     assert result.stderr == stderr
+    assert result.returncode == 4
 
 def test_cli_status_when_repo_doesnt_exist():
     """Test CLI Application ::
@@ -18,5 +18,5 @@ def test_cli_status_when_repo_doesnt_exist():
 
     stderr = b"Error: the repository has not been initialized yet.\nYou can create a new repository using the command:\n\nestate init\n"
     result = sp.run([ESTATE, "status"], stdout=sp.PIPE, stderr=sp.PIPE)
-    assert result.returncode == 2
     assert result.stderr == stderr
+    assert result.returncode == 2
