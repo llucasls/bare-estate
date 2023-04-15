462e369c64bed760c0d1c8e9a46407640c05b3af
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-04-15 17:39:48 -0300

    style: add newline after docstring

--- tests/test_clone.py
+++ tests/test_clone.py
@@ -5,6 +5,7 @@ class TestGitClone:
     "Test estate clone command"
     def test_clone_status(self, mocker):
         "should return status 0 when no error occurs"
+
         mock_run = mocker.patch("subprocess.run")
         mock_run().returncode = 0
 
