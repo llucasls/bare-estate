752742dfeebd3283e3e68d0d2ccbabe64627a864
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-05-25 04:50:55 -0300

    test: test cloned repository

--- tests/test_clone.py
+++ tests/test_clone.py
@@ -1,10 +1,14 @@
 import os
+import tempfile as tmp
+import subprocess as sp
+import tarfile
 
 from bare_estate.commands import clone
 
 
 ESTATE = os.path.join(os.getcwd(), "estate")
 HOME = os.environ["HOME"]
+TARBALL_PATH = "tests/mocks/dotfiles.tar.gz"
 
 
 class TestGitClone:
@@ -20,7 +24,19 @@ class TestGitClone:
     def test_clone_repository(self):
         "should create a bare repository with the same files from a remote repo"
 
-        env = os.environ.copy()
-        #env["BARE_ESTATE_LOCATION"] = tmp_dir
+        with tmp.TemporaryDirectory(dir=os.getcwd()) as tmp_dir:
+            with tarfile.open(TARBALL_PATH, mode="r:gz") as tar:
+                tar.extractall(path=tmp_dir)
 
-        raise NotImplementedError("test clone command")
+            dotfiles_repo = os.path.join(tmp_dir, "dotfiles_repo")
+
+            env = os.environ.copy()
+            env["BARE_ESTATE_LOCATION"] = os.path.join(tmp_dir, "dotfiles")
+
+            proc = sp.run([ESTATE, "clone", dotfiles_repo], env=env,
+                          stdout=sp.PIPE, stderr=sp.PIPE)
+
+            files = set(os.listdir(tmp_dir))
+
+            assert proc.returncode == 0
+            assert files == {"dotfiles", "dotfiles_repo", ".profile"}
