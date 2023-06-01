977a55b3f33b71354a0ddf05879c11bef1ed903d
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-05-31 21:05:21 -0300

    test: create helper method

--- tests/test_init.py
+++ tests/test_init.py
@@ -9,15 +9,19 @@ ESTATE = os.path.join(os.getcwd(), "estate")
 class TestGitInit:
     "Test estate init command"
 
+    def _get_env(self, base_dir):
+        return {
+            *os.environ,
+            "BARE_ESTATE_HISTORY_LOCATION": os.path.join(base_dir, "dotfiles"),
+            "BARE_ESTATE_BASE_DIRECTORY": base_dir,
+        }
+
     def test_initialize_repository(self):
         "should create a new bare repository"
 
         with tmp.TemporaryDirectory(dir=os.getcwd()) as tmp_dir:
 
-            env = os.environ.copy()
-            env["BARE_ESTATE_HISTORY_LOCATION"] = os.path.join(tmp_dir,
-                                                               "dotfiles")
-            env["BARE_ESTATE_BASE_DIRECTORY"] = tmp_dir
+            env = self._get_env(tmp_dir)
 
             init_process = sp.run([ESTATE, "init"], env=env, stdout=sp.PIPE,
                                   stderr=sp.PIPE)
