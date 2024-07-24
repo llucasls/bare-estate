3925a8625545647af1ee369a71ff415bd7aa331b
Lucas L. S. Haine <lucaslshaine@gmail.com>
2024-07-24 14:45:14 -0300

    feat: improve config module

--- bare_estate/config.py
+++ bare_estate/config.py
@@ -2,11 +2,12 @@ import sys
 import os
 import io
 import tomllib
+import typing as t
 
 import toml
 
 
-def join(*paths):
+def join(*paths: str) -> str:
     return os.path.join(*paths).rstrip("/")
 
 
@@ -16,6 +17,10 @@ def home():
 
 def data_home():
     return os.environ.get("XDG_DATA_HOME", join(home(), ".local/share"))
+
+
+def config_home():
+    return os.environ.get("XDG_CONFIG_HOME", join(home(), ".config"))
 
 
 class RepoConfigs:
@@ -40,12 +45,12 @@ class RepoConfigs:
                 (name, self.name, self.git_dir, self.work_tree))
 
     @classmethod
-    def from_name(cls, name: str):
+    def from_name(cls, name: str, git_dir: str, work_tree: str):
         """Create a default repo config from a name."""
         table = {
             "name": name,
-            "git_dir": name,
-            "work_tree": "",
+            "git_dir": join(git_dir, name),
+            "work_tree": join(work_tree, ""),
         }
 
         return cls(table)
@@ -53,44 +58,69 @@ class RepoConfigs:
 
 class Configs:
     repos: dict[str, RepoConfigs]
+    filename: str
 
     def __init__(self, config_file=None):
         """Read and parse a TOML configuration file."""
         if config_file is None:
-            configs = self.default
+            self.filename = join(config_home(), "bare_estate.toml")
+            try:
+                with open(self.filename) as file:
+                    configs = self._read_toml(file)
+            except (FileNotFoundError, PermissionError):
+                configs = self.default
+        elif isinstance(config_file, str):
+            configs = self._read_toml(config_file)
+            self.filename = config_file
         else:
             configs = self._read_toml(config_file)
+            self.filename = join(config_home(), "bare_estate.toml")
 
         self._configs = {}
+        gen_configs = configs.get("config", {})
 
-        self.base_dir = configs.get("base_dir", home())
+        self.base_dir = gen_configs.get("base_dir", home())
 
-        self.git_dir = configs.get("git_dir", join(data_home(), "bare_estate"))
+        self.git_dir = gen_configs.get(
+            "git_dir", join(data_home(), "bare_estate")
+        )
         if self.git_dir == "":
             raise ValueError("git_dir cannot be empty")
 
-        self.work_tree = configs.get("work_tree", "")
+        self.work_tree = gen_configs.get("work_tree", "")
 
-        self.repos = {
-            t["name"]: RepoConfigs(t) for t in configs.get("repo", [])
-        }
+        self.repos = {}
 
-    def __iter__(self):
-        """Yield the names of all configuration repositories."""
-        for repo in self.repos.keys():
-            yield repo
+        for table in configs.get("repo", []):
+            if "name" not in table:
+                raise TypeError("new repository must have a name")
+
+            repo_config = {
+                "name": table["name"],
+                "git_dir": table.get("git_dir", table["name"]),
+                "work_tree": table.get("work_tree", ""),
+            }
+            self.repos[table["name"]] = RepoConfigs(repo_config)
 
     @classmethod
-    def from_dict(cls, config_dict):
+    def from_dict(cls, config_dict: dict[str, t.Any]):
         """Create a new Configs instance from a dictionary."""
         conf = cls()
         default = conf.default
-        conf.base_dir = config_dict.get("base_dir", default["base_dir"])
-        conf.git_dir = config_dict.get("git_dir", default["git_dir"])
-        conf.work_tree = config_dict.get("work_tree", default["work_tree"])
+        gen_configs = config_dict.get("config", {})
+        conf.base_dir = gen_configs.get("base_dir", default["base_dir"])
+        conf.git_dir = gen_configs.get("git_dir", default["git_dir"])
+        conf.work_tree = gen_configs.get("work_tree", default["work_tree"])
 
         repos = config_dict.get("repo", config_dict.get("repos", {}))
         for repo_configs in repos:
+            if "name" not in repo_configs:
+                raise TypeError("new repository must have a name")
+
+            name = repo_configs["name"]
+            repo_configs["git_dir"] = repo_configs.get("git_dir", name)
+            repo_configs["work_tree"] = repo_configs.get("work_tree", "")
+
             conf.create_repo(repo_configs)
 
         return conf
@@ -162,17 +192,49 @@ class Configs:
             "repo": [],
         }
 
-    def get(self, name: str) -> RepoConfigs:
-        """Return a config object for a particular repository."""
+    def get(self, name: str, key=None) -> RepoConfigs | str | None:
         if name not in self.repos:
-            self.repos[name] = RepoConfigs.from_name(name)
-        return self.repos[name]
+            return None
+        elif key is None:
+            return self.repos[name]
+
+        return join(getattr(self, key, ""), getattr(self.repos[name], key))
+
+    def dump(self, file: io.TextIOBase):
+        """Write TOML configuration into a file-like object."""
+        repos = []
+
+        for r in self.repos.values():
+            repo: dict[str, str] = {"name": r.name}
+            if r.git_dir != r.name:
+                repo["git_dir"] = r.git_dir
+            if r.work_tree != "":
+                repo["work_tree"] = r.work_tree
+            repos.append(repo)
+
+        c: dict[str, t.Any] = {"config": {"base_dir": self.base_dir}}
+        if self.git_dir != self.default["git_dir"]:
+            c["config"]["git_dir"] = self.git_dir
+        if self.work_tree != self.default["work_tree"]:
+            c["config"]["work_tree"] = self.work_tree
+
+        toml.dump(c, file)
+        file.write("\n")
+
+        if len(repos) > 0:
+            file.write(toml.dumps({"repo": repos}).strip())
+            file.write("\n")
 
     def create_repo(self, table: dict[str, str]):
         if "name" not in table:
             raise TypeError("new repository must have a name")
 
         self.repos[table["name"]] = RepoConfigs(table)
+
+    def create_config_file(self):
+        os.makedirs(os.path.dirname(self.filename), exist_ok=True)
+        with open(self.filename, mode="x") as file:
+            self.dump(file)
 
     def _read_toml(self, input_file):
         if isinstance(input_file, str):
@@ -184,6 +246,7 @@ class Configs:
             except PermissionError:
                 print(f"Warning: The file {input_file} doesn't allow reading",
                       "Using default configuration as fallback",
+                      sep="\n",
                       file=sys.stderr)
                 return self.default
         elif not isinstance(input_file, io.IOBase):
